9748fb6fab8723915b937bae133d5b263d124049
bluebirdsky <dnippa@pillartechnology.com>
2017-10-08 22:40:21 -0400

    apply dynamic server-side rendering

---
+++ server.ts
@@ -0,0 +1,35 @@
+require('zone.js/dist/zone-node');
+
+console.log('starting universal server...');
+import * as express from 'express';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+
+const {
+  ServerAppModuleNgFactory
+} = require('./dist-server/main.bundle');
+
+const app = express();
+
+app.engine(
+  'html',
+  ngExpressEngine({
+    bootstrap: ServerAppModuleNgFactory,
+  })
+);
+
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+app.use(express.static(__dirname + '/assets', { index: false }));
+app.use(express.static(__dirname + '/dist', { index: false }));
+
+app.get('/*', (req, res) => {
+  console.log('GET: ${req.originalUrl}');
+  res.render('./dist/index', {
+    req: req,
+    res: res
+  });
+  console.log('GET: ${req.originalUrl}');
+});
+
+app.listen(process.env.PORT || 8080, () => {});
